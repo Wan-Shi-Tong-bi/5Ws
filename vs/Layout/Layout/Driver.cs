407ff6ddd31d07ab69efe132953033b9f667be2f
Wan-Shi-Tong-bi <wanshitong.bi@gmail.com>
2021-10-19 14:41:09 +0200

    added wpf layout exe

---
+++ vs/Layout/Layout/Driver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layout
+{
+    class Driver
+    {
+        public string DriverName { get; set; }
+        public Car DriverCar { get; set; }
+
+        public override string ToString()
+        {
+            return $"{DriverName}: {DriverCar.Brand} {DriverCar.PS} PS";
+        }
+    }
+}
