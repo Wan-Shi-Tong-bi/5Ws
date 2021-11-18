c7a9f872e018e9eb23c7e89e172e013220d6ae7f
Wan-Shi-Tong-bi <wanshitong.bi@gmail.com>
2021-11-18 07:43:57 +0100

    changed test pro

---
+++ vs/wpf/test/UebenFuerTest/UebenFuerTest/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UebenFuerTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Point> points = new List<Point>();
+        private double origWidth = 0;
+        private double origHigh = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void patOpenMI_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void patSaveMI_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(krankCanv);
+            points.Add(p);
+            Line line = new Line();
+            if(points.Count > 1)
+            {
+                line = new Line
+                {
+                    X1 = points[points.Count - 2].X,
+                    X2 = points[points.Count - 1].X,
+                    Y1 = points[points.Count - 2].Y,
+                    Y2 = points[points.Count - 1].Y,
+                    Stroke = new SolidColorBrush(Colors.Red)
+                };
+                Panel.SetZIndex(line, points.Count);
+                krankCanv.Children.Add(line);
+            }
+            origHigh = krankCanv.ActualHeight;
+            origWidth = krankCanv.ActualWidth;
+            
+        }
+
+        private void drawLine()
+        {
+            
+            krankCanv.Children.Clear();
+            
+            List<Point> pointlist = new List<Point>();
+            foreach(var point in points)
+            {
+                Point p = new Point
+                {
+                    X = point.X * krankCanv.ActualWidth / origWidth,
+                    Y = point.Y * krankCanv.ActualHeight / origHigh
+                };
+                pointlist.Add(p);
+            }
+            for(int i = 1; i < pointlist.Count; i++)
+            {
+                Line line = new Line
+                {
+                    X1 = pointlist[i - 1].X,
+                    X2 = pointlist[i].X,
+                    Y1 = pointlist[i-1].Y,
+                    Y2 = pointlist[i].Y,
+                    Stroke = new SolidColorBrush(Colors.Red)
+                };
+                Panel.SetZIndex(line, points.Count);
+                krankCanv.Children.Add(line);
+            }
+            points = pointlist;
+            origHigh = krankCanv.ActualHeight;
+            origWidth = krankCanv.ActualWidth;
+
+
+        }
+
+        private void krankCanv_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            drawLine();
+        }
+
+        
+    }
+}
---
+++ vs/wpf/test/UebenFuerTest/UebenFuerTest/Patient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UebenFuerTest
+{
+    class Patient
+    {
+        public int svnr { get; set; }
+        public string vorname { get; set; }
+        public string nachname { get; set; }
+        public DateTime birthday { get; set; }
+        public List<Krankheit> krankheiten { get; set; }
+
+        public static bool TryParse(string line, out Patient p)
+        {
+            p = new Patient();
+            string[] words = line.Split(';');
+            int sv = int.Parse(words[0]);
+            string vname = words[1];
+            string lname = words[2];
+            DateTime bday = DateTime.Parse(words[3]);
+            List<Krankheit> krankheiten = new List<Krankheit>();
+            if(words.Length >= 4)
+            {
+                
+            }
+            return false;
+        }
+            
+    }
+}
