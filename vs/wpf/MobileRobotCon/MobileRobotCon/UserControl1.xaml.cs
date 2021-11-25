39ebae6526a031fd4315a3d73e2451fe507b91f0
Wan-Shi-Tong-bi <wanshitong.bi@gmail.com>
2021-11-25 11:58:35 +0100

    added mgin 25.11

---
+++ vs/wpf/MobileRobotCon/MobileRobotCon/UserControl1.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MobileRobotCon
+{
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+
+        private string groupHead;
+        [Category("MyUserControlData"), Description("Vlaue of the thing")]
+        public string GroupHeader
+        {
+            get { return groupHead; }
+            set
+            {
+                groupHead = value;
+                gbHeader.Header = groupHead;
+            }
+        }
+
+        private double sliVal;
+
+        public double SliVal
+        {
+            get { return sliVal; }
+            set
+            {
+                sliVal = value;
+                proForward.Value = sliVal;
+                proBack.Value = sliVal * -1;
+                sliBackFor.Value = sliVal;
+            }
+        }
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            SliVal = sliBackFor.Maximum;
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            SliVal = sliBackFor.Minimum;
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            SliVal = 0;
+        }
+
+        private void sliBackFor_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SliVal = sliBackFor.Value;
+        }
+
+        private void proBack_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SliVal = (e.GetPosition(proBack).Y / proBack.ActualHeight) - 1;
+        }
+
+        private void proForward_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SliVal = 1 - e.GetPosition(proForward).Y / proBack.ActualHeight;
+        }
+    }
+}
---
+++ vs/wpf/Usercontrol_Tacho_Analoganzeige/UsercontrolTacho/UserControl1.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UsercontrolTacho
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        private double radius;
+        private double NEEDLE_LENGTH = 0.75;
+        private double TACHO_MIN = 0.80;
+        private double TACHO_MAX = 0.85;
+        private double BORDER_LENGTH = 0.05;
+        private Line needle_line;
+
+        private double tachoVar;
+
+        [Category("TachoValueData"),Description("Value of Tacho")]
+        public double TachoValue
+        {
+            get { return tachoVar; }
+            set { tachoVar = value;
+                DrawNeedle();
+                lblValue.Content = tachoVar;
+                PrgBar.Value = tachoVar / 180 * 100;
+                SliderTacho.Value = tachoVar;
+            }
+        }
+
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void CanTacho_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            CanTacho.Children.Clear();
+            radius = CanTacho.ActualWidth / 2 <= CanTacho.ActualHeight ? CanTacho.ActualWidth / 2 : CanTacho.ActualHeight;
+            DrawTacho();
+            DrawNeedle();
+
+        }
+
+
+        private void SliderTacho_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+           TachoValue = SliderTacho.Value;
+        }
+
+        private void DrawNeedle()
+        {
+            //Spline statt Strich
+            CanTacho.Children.Remove(needle_line);
+            needle_line = new Line();
+            needle_line.X1 = 0;
+            needle_line.Y1 = 0;
+            needle_line.X2 = -radius * NEEDLE_LENGTH;
+            needle_line.Y2 = 0;
+            needle_line.Stroke = new SolidColorBrush(Colors.Red);
+
+            double val = SliderTacho.Value;
+
+            TransformGroup tg = new TransformGroup();
+            tg.Children.Add(new RotateTransform(val));
+            tg.Children.Add(new TranslateTransform(0, -radius * BORDER_LENGTH));
+            tg.Children.Add(new TranslateTransform(CanTacho.ActualWidth / 2, CanTacho.ActualHeight));
+
+            needle_line.RenderTransform = tg;
+            CanTacho.Children.Add(needle_line);
+        }
+
+        private void DrawTacho()
+        {
+            Brush whiteBrush = new SolidColorBrush(Colors.White);
+            for (int i = 0; i <= 18; i++)
+            {
+                Line line = new Line();
+                line.X1 = -radius * TACHO_MIN;
+                line.Y1 = 0;
+                line.X2 = -radius * TACHO_MAX;
+                line.Y2 = 0;
+                line.Stroke = whiteBrush;
+
+                TransformGroup tg = new TransformGroup();
+                tg.Children.Add(new RotateTransform(10 * i));
+                tg.Children.Add(new TranslateTransform(0, -radius * BORDER_LENGTH));
+                tg.Children.Add(new TranslateTransform(CanTacho.ActualWidth / 2, CanTacho.ActualHeight));
+
+                line.RenderTransform = tg;
+
+                CanTacho.Children.Add(line);
+
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = "" + (i * 10);
+                textBlock.Foreground = whiteBrush;
+                tg = new TransformGroup();
+
+                tg.Children.Add(new TranslateTransform(-10, -10));
+                tg.Children.Add(new RotateTransform(-10 * i));
+                tg.Children.Add(new TranslateTransform(-radius * 0.9, 0));
+                tg.Children.Add(new RotateTransform(10 * i));
+                tg.Children.Add(new TranslateTransform(CanTacho.ActualWidth / 2, CanTacho.ActualHeight));
+                tg.Children.Add(new TranslateTransform(0, -radius * BORDER_LENGTH));
+
+
+                textBlock.RenderTransform = tg;
+                CanTacho.Children.Add(textBlock);
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            radius = CanTacho.ActualWidth / 2 <= CanTacho.ActualHeight ? CanTacho.ActualWidth / 2 : CanTacho.ActualHeight;
+            DrawTacho();
+            DrawNeedle();
+        }
+    }
+}
+
